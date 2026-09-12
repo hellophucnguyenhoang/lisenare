c2fd81662fbc8c881de90dd0f919c19ac0e44ef2
Phuc Nguyen Hoang <hellophucnguyenhoang@gmail.com>
2026-09-12 14:03:55 +0700

    ref: just to have a commit

--- testt_semantic_sim.py
+++ testt_semantic_sim.py
@@ -4,5 +4,6 @@ sentences = ["What is your age", "How old are you"]
 
 model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
 embeddings = model.encode(sentences, convert_to_tensor=True)
+
 cosine_score = util.cos_sim(embeddings[0], embeddings[1])
 print(cosine_score.item())
